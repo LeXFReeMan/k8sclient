426a83d06bd048a1473307351516999e4d88b176
Alexander Ivanov <alexivanov@nic.ru>
2023-05-08 22:34:20 +0300

    update k8s version to 1.27.1

--- pkg/k8sclient/clients.go
+++ pkg/k8sclient/clients.go
@@ -113,7 +113,11 @@ func NewClients(config ClientsConfig) (*Clients, error) {
 		// we want to separate client and manager. Thus we configure the client here
 		// properly on our own instead of relying on the manager to provide a
 		// client, which might change in the future.
-		mapper, err := apiutil.NewDynamicRESTMapper(rest.CopyConfig(restConfig))
+		httpClient, err := rest.HTTPClientFor(rest.CopyConfig(restConfig))
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+		mapper, err := apiutil.NewDynamicRESTMapper(rest.CopyConfig(restConfig), httpClient)
 		if errors.Is(err, context.DeadlineExceeded) {
 			return nil, microerror.Mask(timeoutError)
 		} else if err != nil {
